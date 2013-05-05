135d644baad36b640372a7580e9139e8a7d667b2
cesine <cesine@yahoo.com>
2013-05-05 15:17:39 -0400

    checking in all the libraries for  v3.2.9

---
+++ TouchDB-Android/obj/local/armeabi-v7a/objs/com_couchbase_touchdb_TDCollateJSON/com_couchbase_touchdb_TDCollateJSON.o.d
@@ -0,0 +1,85 @@
+obj/local/armeabi-v7a/objs/com_couchbase_touchdb_TDCollateJSON/com_couchbase_touchdb_TDCollateJSON.o: \
+ jni/com_couchbase_touchdb_TDCollateJSON.cpp \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/strings.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/memory.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/dlfcn.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/ctype.h \
+ jni/sqlite3.h jni/com_couchbase_touchdb_TDCollateJSON.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/log.h
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/memory.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/dlfcn.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/ctype.h:
+
+jni/sqlite3.h:
+
+jni/com_couchbase_touchdb_TDCollateJSON.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/log.h:
--- TouchDB-Android/obj/local/x86/objs/com_couchbase_touchdb_TDCollateJSON/com_couchbase_touchdb_TDCollateJSON.o.d
+++ TouchDB-Android/obj/local/x86/objs/com_couchbase_touchdb_TDCollateJSON/com_couchbase_touchdb_TDCollateJSON.o.d
@@ -1,86 +1,88 @@
-/Users/mschoch/Documents/research/cbl/TouchDB-Android/TouchDB-Android/obj/local/x86/objs/com_couchbase_touchdb_TDCollateJSON/com_couchbase_touchdb_TDCollateJSON.o: \
- /Users/mschoch/Documents/research/cbl/TouchDB-Android/TouchDB-Android/jni/com_couchbase_touchdb_TDCollateJSON.cpp \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/stdio.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/_types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/machine/_types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/asm/types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/types.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/stdint.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/stdlib.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/string.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/malloc.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/alloca.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/strings.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/memory.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/dlfcn.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/ctype.h \
- /Users/mschoch/Documents/research/cbl/TouchDB-Android/TouchDB-Android/jni/sqlite3.h \
- /Users/mschoch/Documents/research/cbl/TouchDB-Android/TouchDB-Android/jni/com_couchbase_touchdb_TDCollateJSON.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/jni.h \
- /Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/android/log.h
+obj/local/x86/objs/com_couchbase_touchdb_TDCollateJSON/com_couchbase_touchdb_TDCollateJSON.o: \
+ jni/com_couchbase_touchdb_TDCollateJSON.cpp \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/stdio.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/cdefs.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/android/api-level.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/stdint.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/machine/_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/posix_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/stddef.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/compiler.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/asm/posix_types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/asm/posix_types_32.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/asm/types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/types.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/machine/kernel.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/sysmacros.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/stdlib.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/string.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/malloc.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/alloca.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/strings.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/memory.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/dlfcn.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/ctype.h \
+ jni/sqlite3.h jni/com_couchbase_touchdb_TDCollateJSON.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/jni.h \
+ /Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/android/log.h
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/stdio.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/stdio.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/cdefs.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/cdefs_elf.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/android/api-level.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/stdint.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/_types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/machine/_types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/posix_types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/stddef.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/compiler.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/asm/types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/asm/posix_types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/types.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/asm/posix_types_32.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/stdint.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/asm/types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/linux/types.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/machine/kernel.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/stdlib.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/sys/sysmacros.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/string.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/stdlib.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/malloc.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/string.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/alloca.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/malloc.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/strings.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/alloca.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/memory.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/strings.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/dlfcn.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/memory.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/ctype.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/dlfcn.h:
 
-/Users/mschoch/Documents/research/cbl/TouchDB-Android/TouchDB-Android/jni/sqlite3.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/ctype.h:
 
-/Users/mschoch/Documents/research/cbl/TouchDB-Android/TouchDB-Android/jni/com_couchbase_touchdb_TDCollateJSON.h:
+jni/sqlite3.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/jni.h:
+jni/com_couchbase_touchdb_TDCollateJSON.h:
 
-/Users/mschoch/android-ndk-r8b/platforms/android-9/arch-x86/usr/include/android/log.h:
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/jni.h:
+
+/Users/gina/android-ndk-r8e/platforms/android-14/arch-x86/usr/include/android/log.h:
